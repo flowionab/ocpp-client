12e1c14460e0a8debcb9730d36c8983674826d31
Joatin Granlund <joatin@MacBookPro.localdomain>
2024-12-02 23:42:13 +0100

    add missing export

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub mod ocpp_2_0_1;
 pub use rust_ocpp;
 
 pub use self::connect::connect;
+pub use self::connect::ConnectOptions;
 
 #[cfg(feature = "ocpp_1_6")]
 pub use self::connect::connect_1_6;
